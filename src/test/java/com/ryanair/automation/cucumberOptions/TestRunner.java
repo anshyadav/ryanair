b228fe32098d20a11c6e3075c66003d839c7832d
anshyadav <yadavansh04@gmail.com>
2021-04-12 20:57:39 +0100

    Test Runner update for glue and feature paths

--- src/test/java/com/ryanair/automation/cucumberOptions/TestRunner.java
+++ src/test/java/com/ryanair/automation/cucumberOptions/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", tags = "@SmokeTest", plugin = {"pretty", "html:target/cucumber/Test Report.html"}, monochrome = true)
+@CucumberOptions(features = "src/test/java/com/ryanair/automation/features", glue = "com.ryanair.automation.stepDefinitions", tags = "@SmokeTest", plugin = {"pretty", "html:target/cucumber/Test Report.html"}, monochrome = true)
 public class TestRunner {
 
 }
